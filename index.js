b840a06539a0efdb1c2b9d571dcb7e6965aa6ae4
Anand <anand.work2005@gmail.com>
2025-08-20 21:56:19 +0530

    setting up the project

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express();
+const PORT = process.env.PORT ?? 6969;
+
+app.get('/', (req, res) => {
+  return res.json({ status: `Server is up and running` });
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
+
